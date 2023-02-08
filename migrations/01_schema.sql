60bf87e9f60bb90be1e4d091b4c6b60dc73101f0
Jonathan Phair <jmphair@gmail.com>
2023-02-07 20:22:37 -0500

    add properties and property reviews tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users, reservations CASCADE;
+DROP TABLE IF EXISTS users, reservations, properties, property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -14,3 +14,32 @@ CREATE TABLE reservations (
   property_id INTEGER,
   guest_id INTEGER
 );
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER,
+  property_id INTEGER,
+  reservation_id INTEGER,
+  rating SMALLINT,
+  message TEXT
+);
+
